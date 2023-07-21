2c5a9eacb1f46625e1afd7a86f513f4189180589
revivalmhlanga <revivalmhlanga22@gmail.com>
2023-07-21 02:25:04 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - func checks for a digit (0 through).
+ * @a: a - Variable
+ * @b: b - variable
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
