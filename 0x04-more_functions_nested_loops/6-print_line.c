f3d5622178f0d78095b4b89c4bcd1171f4e7015e
revivalmhlanga <revivalmhlanga22@gmail.com>
2023-07-21 03:49:44 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,14 +2,15 @@
 
 /**
  * print_line - func draws a straight line in the terminal
- * @n: number of times the character _ should be printed 
+ * @n: number of times the character _ should be printed											    
  */
+ 
 void print_line(int n)
 {
 	if (n <= 0)
 	{
 		_putchar('\n');
-	}else
+	} else
 	{
 		int i;
 
