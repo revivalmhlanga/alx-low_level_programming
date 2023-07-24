2719242c83de8da46f848febdda37ee8d67e4451
revivalmhlanga <revivalmhlanga22@gmail.com>
2023-07-24 09:14:25 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#includec"main.h"
+/**
+ * print_rev -imprime en reversa
+ * @s: string
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int longi = 0;
+	int o;
+
+	while (*s != '\0')
+	{
+		long++;
+		s++;
+	}
+	S--;
+	for (o = longi; o > 0; O--)
+	{
+		putchar(*s);
+		S--;
+	}
+	putchar('\n');
+}
